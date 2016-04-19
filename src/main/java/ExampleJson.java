ddf495115b2710f0eb94754b0018e573671a0f73
Marin_Mravec <P3502441@L3505100689.ness.com>
2016-04-19 16:23:37 +0200

    weather for next day

--- src/main/java/ExampleJson.java
+++ src/main/java/ExampleJson.java
@@ -21,7 +21,6 @@ public class ExampleJson {
 
 
     public static void main(String[] args) {
-
         String result = "";
 
         try {
@@ -76,6 +75,10 @@ public class ExampleJson {
             int result_id = JSONObject_list.getInt("dt");
             String result_date = JSONObject_list.getString("dt_txt");
 
+            String[] parts = result_date.split(" ",2);
+            String part1 = parts[0];
+
+
             //cord
             double reusult_lon = city.getJSONObject("coord").getDouble("lon");
             double result_lat = city.getJSONObject("coord").getDouble("lat");
@@ -99,27 +102,108 @@ public class ExampleJson {
             double result_wind_deg = JSONObject_list.getJSONObject("wind").getDouble("deg");
 
             //rain
-            double result_rain_per_tree_hour = JSONObject_list.getJSONObject("rain").getDouble("3h");
-
-            result_weather = "Mesto: " + city.getString("name") + "\t Koordinaty: [" + reusult_lon + " " + result_lat + " ]\t Populacia:\t " + result_population + " \n" +
-                    "Aktualna teplota: " + String.format("%.2f",result_temp) + " C \tVlhkost: " + result_humidity + "%\t Max/Min teplota: " + String.format("%.2f",result_temp_max) + "/" + String.format("%.2f",result_temp_min) + " C" +
-                    "\nTlak: " + result_pressure + " hPa\t Rychlost vetra: " + result_wind_speed + " m/s\t Mnozstvo zrazok zaposledne 3h " + result_rain_per_tree_hour + " mil" +
-                    "\nposledny update: " + result_date;
-
-
-//            if (result_clauds >= 75 && result_clauds <= 100) {
-//                System.out.println("Oblacno");
-//            } else if (result_clauds < 75 && result_clauds > 35) {
-//                System.out.println("Polooblacno");
+//            double result_rain_per_tree_hour;
+//            if (!JSONObject_list.has("rain")) {
+//                result_rain_per_tree_hour = 0.0;
 //            } else {
-//                System.out.println("Slnecno");
+//                result_rain_per_tree_hour = JSONObject_list.getJSONObject("rain").getDouble("3h");
 //            }
+
+            //String result_weather = null;
+            JSONArray JSONArray_list_next_day = jsonObject.getJSONArray("list");
+            if (JSONArray_list.length() > 0) {
+                JSONObject JSONObject_list_next_day = JSONArray_list_next_day.getJSONObject(8);
+
+                //name
+                String result_name_next_day = city.getString("country");
+                double result_population_next_day = city.getDouble("population");
+
+                String result_date_next_day = JSONObject_list_next_day.getString("dt_txt");
+
+                //cord
+                double reusult_lon_next_day = city.getJSONObject("coord").getDouble("lon");
+                double result_lat_next_day = city.getJSONObject("coord").getDouble("lat");
+
+                //weather
+                double result_temp_next_day = (JSONObject_list_next_day.getJSONObject("main").getDouble("temp") - 273.15);
+                double result_humidity_next_day = JSONObject_list_next_day.getJSONObject("main").getDouble("humidity");
+                double result_pressure_next_day = JSONObject_list_next_day.getJSONObject("main").getDouble("pressure");
+                double result_temp_max_next_day = (JSONObject_list_next_day.getJSONObject("main").getDouble("temp_max") - 273.15);
+                double result_temp_min_next_day = (JSONObject_list_next_day.getJSONObject("main").getDouble("temp_min") - 273.15);
+
+                //clouds
+                double result_clauds_next_day = JSONObject_list.getJSONObject("clouds").getDouble("all");
+
+                //wind
+                double result_wind_speed_next_day = JSONObject_list_next_day.getJSONObject("wind").getDouble("speed");
+                double result_wind_deg_next_day = JSONObject_list_next_day.getJSONObject("wind").getDouble("deg");
+
+                //rain
+//                double result_rain_per_tree_hour_next_day;
+//                if (!JSONObject_list_next_day.isNull("rain")) {
+//                    result_rain_per_tree_hour_next_day = 0.0;
+//                } else {
+//                    result_rain_per_tree_hour_next_day = JSONObject_list_next_day.getJSONObject("rain").getDouble("3h");
+//                }
+
+
+
+
+
+
+                //String result_weather = null;
+                JSONArray JSONArray_list_night = jsonObject.getJSONArray("list");
+                if (JSONArray_list_night.length() > 0) {
+                    JSONObject JSONObject_list_night = JSONArray_list_night.getJSONObject(4);
+
+                    double result_temp_max_night = (JSONObject_list_night.getJSONObject("main").getDouble("temp_max") - 273.15);
+                    double result_temp_min_night = (JSONObject_list_night.getJSONObject("main").getDouble("temp_min") - 273.15);
+
+                    double result_clauds_night = JSONObject_list_night.getJSONObject("clouds").getDouble("all");
+
+                    String result_date_night = JSONObject_list_night.getString("dt_txt");
+
+
+                    String result_night;
+                    if (result_clauds_night >= 75 && result_clauds_night <= 100) {
+                        result_night = ("Oblacno");
+                    } else if (result_clauds_night < 75 && result_clauds_night > 35) {
+                        result_night = ("Polooblacno");
+                    } else {
+                        result_night = ("Slnecno");
+                    }
+                    //System.err.print("Update: " + result_date_night +" Oblcnst: " + result_night + "\n Nocna teplota MAX/MIN: " + String.format("%.2f", result_temp_max_night) + " C/" + String.format("%.2f", result_temp_min_night) + " C " );
+
+                    String result;
+                    if (result_clauds >= 75 && result_clauds <= 100) {
+                        result = ("Oblacno");
+                    } else if (result_clauds < 75 && result_clauds > 35) {
+                        result = ("Polooblacno");
+                    } else {
+                        result = ("Slnecno");
+                    }
+
+                    if (result_clauds_next_day >= 75 && result_clauds_next_day <= 100) {
+                        result = ("Oblacno");
+                    } else if (result_clauds_next_day < 75 && result_clauds_next_day > 35) {
+                        result = ("Polooblacno");
+                    } else {
+                        result = ("Slnecno");
+                    }
+
+                    result_weather = "*************Predpoved na Dnes [" + part1 + "]************* \n\nMesto: " + city.getString("name") + "\t Koordinaty: [" + reusult_lon + " " + result_lat + "]\t Populacia:\t " + result_population + " \n" +
+                            "Aktualna teplota: " + String.format("%.2f", result_temp) + " C \tVlhkost: " + result_humidity + "%\t Max/Min teplota: " + String.format("%.2f", result_temp_max) + "/" + String.format("%.2f", result_temp_min) + " C Obloha: " + result +
+//                            "\nTlak: " + result_pressure + " hPa\t Rychlost vetra: " + result_wind_speed + " m/s\t Mnozstvo zrazok zaposledne 3h " + result_rain_per_tree_hour + " mil" +
+                            "\nposledny update: " + result_date + "\n\n" +
+                            "*************Predpoved na zajtra************* \n" +
+                            "\nMesto:" + city.getString("name") + "t Koordinaty: [" + reusult_lon + " " + result_lat + "]\t Populacia:\t " + result_population + " \n" +
+                            "Aktualna teplota: " + String.format("%.2f", result_temp_next_day) + " C \tVlhkost: " + result_humidity_next_day + "%\t Max/Min teplota: " + String.format("%.2f", result_temp_max_next_day) + "/" + String.format("%.2f", result_temp_min) + " C Obloha: " + result +
+                            "\nTlak: " + result_pressure_next_day + " hPa\t Rychlost vetra: " + result_wind_speed_next_day + " m/s\t Mnozstvo zrazok zaposledne 3h " + //result_rain_per_tree_hour_next_day + " mil" +
+                            "\nposledny update: " + result_date_next_day;
+                }
+            }
         }
-
-            //System.out.println("Mesto:" + city.getString("name") + " populacia: " + city.getDouble("population") + " skratka: " + city.getString("country") );
-            //System.out.print("temp:" + list.getDouble("list.temp"));
             return result_weather;
-
         }
     }
 
